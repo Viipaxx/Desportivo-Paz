62f25903896371872161aa71cfb8afc87efa1564
Vitor Paz <kngatuno@gmail.com>
2023-05-09 04:54:47 -0300

    ajeitando tudo

--- js/buy.js
+++
@@ -1,8 +0,0 @@
-const buy = document.getElementById('buy')
-
-buy.addEventListener('click', () => {
-    alert('Você está sendo redirecionado para o chat com o vendedor!')
-    const red = document.getElementById('red')
-    red.href = 'https://wa.me/+5581997806196?text=Olá,%20gostaria%20de%20comprar%20uma%20bmeia%20antiderrapante'
-})
-
