e045bed7f6887b619956b3dfade6380c3230ae34
Vitor Gabriel Paz de Santana <1221322483@acd.grupotiradentes.com>
2023-05-10 08:59:36 -0300

    alterando arquivos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
 
-
 async function getProduct(){
-    const url = '../../dados.json'
+    const url = "../../data/products.json"
     const fetching = await fetch(url)
     const products = await fetching.json()
     return await products.produtos
