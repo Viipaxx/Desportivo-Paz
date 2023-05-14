b21cc9a1d89587028590400dab2c23f689725831
Vitor Paz <101137538+Viipaxx@users.noreply.github.com>
2023-05-14 13:14:19 -0300

    update

--- assets/js/trocaImagem.js
+++ assets/js/trocaImagem.js
@@ -13,19 +13,16 @@ pegaImagem = () => {
 trocaImagemPrincipal = (path, id) => {
 
     const imagemPrincipal = document.querySelector('#img-prod')
-    const pathImagemPrincipal = imagemPrincipal.src
     imagemPrincipal.src = path
 
-    trocaImagemPequena(pathImagemPrincipal, id)
-
 
 }
 
-trocaImagemPequena = (path, id) => {
-    const cores = document.querySelectorAll('.colors img')
-    cores.forEach((e, s) => {
-        if (s === id){
-            e.src = path
-        }       
-    })
-}
+// trocaImagemPequena = (path, id) => {
+//     const cores = document.querySelectorAll('.colors img')
+//     cores.forEach((e, s) => {
+//         if (s === id){
+//             e.src = path
+//         }       
+//     })
+// }
