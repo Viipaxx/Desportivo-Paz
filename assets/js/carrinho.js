dc96b3102c4a7ba404251966d31c4e8025436f51
Vitor Paz <101137538+Viipaxx@users.noreply.github.com>
2023-05-16 17:19:34 -0300

    update

--- assets/js/carrinho.js
+++ assets/js/carrinho.js
@@ -1,4 +1,7 @@
 const carrinho = document.querySelector('.carrinho i')
+const qtd = document.querySelector('#qtd-carrinho')
+const btn = document.querySelectorAll('.btn-qtd-carrinho')
+
 
 carrinho.addEventListener('click', () => {
     const carrinhoCompra = document.querySelector('.carrinho-compra')
@@ -6,3 +9,43 @@ carrinho.addEventListener('click', () => {
     carrinho.classList.toggle('fa-cart-shopping')
     carrinho.classList.toggle('fa-x')
 })
+
+
+btn.forEach((e) => {
+    e.addEventListener('click', (element) => {
+        if (element.target.textContent === '-'){
+            rem1Item()
+        }else if (element.target.textContent === '+') {
+            add1Item()
+        }
+        else{
+
+        }
+    })
+})
+
+add1Item = () => {
+    qtd.innerHTML = parseInt(qtd.textContent) + 1
+}
+
+rem1Item = () => {
+   if (parseInt(qtd.textContent) === 0){
+   }else{
+    qtd.innerHTML = parseInt(qtd.textContent) - 1
+    } 
+}
+
+remItem =() => {
+    qtd.innerHTML = 0
+}
+
+setProdutosCarrinho = (produto) =>{
+    
+}
+
+(async () => {
+    const produtos = await getProduct();
+    setTimeout(() => {
+        
+    }, 1000);
+  })();
